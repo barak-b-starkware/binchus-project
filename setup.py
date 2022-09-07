607e3d9c4ab1ce968715a5e2bccb203208814f50
Barak <barakb@starkware.co>
2022-09-07 11:01:20 +0300

    check if travis builds something

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'binchus-project',
     version = '0.1.0',
-    author = 'Barak Basson',
+    author = 'Barak Bassonn',
     description = 'A project in Dan Gittik\'s course.',
     packages = find_packages(),
     install_requires = ['click', 'flask'],
