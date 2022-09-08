ddbbc3794913c0d172a36534d99d23ad5d1a50be
Barak <barakb@starkware.co>
2022-09-08 19:59:16 +0300

    changing back

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 function main {
     python -m virtualenv .env --prompt "[binchus-project] "
     find .env -name site-packages -exec bash -c 'echo "../../../../" > {}/self.pth' \;
-    find .env -name site-packages -exec bash -c 'echo "../../../../binchus" > {}/self.pth' \;
     .env/bin/pip install -U pip
     .env/bin/pip install -r requirements.txt
 }
